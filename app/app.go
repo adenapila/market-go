77f34e4c10e2250285c6e08dfeb0d3aeed773f1f
adenapila <adenapila@gmail.com>
2021-01-27 10:37:46 +0700

    mux

--- app/app.go
+++ app/app.go
@@ -1,16 +1,31 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-func Start()  {
-	//define routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
+func Start() {
+	//mux := http.NewServeMux()
+	router := mux.NewRouter()
+
+	//define routes - matcher
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomers).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	//starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe("localhost:8888", router))
 }
 
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
+}
+
+func createCustomers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post request received!")
+}
