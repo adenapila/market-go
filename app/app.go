ea5f234f006a8ccb6022af23db1768e712b77879
adenapila <adenapila@gmail.com>
2021-02-08 21:19:30 +0700

    port err 2

--- app/app.go
+++ app/app.go
@@ -1,9 +1,9 @@
 package app
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/adenapila/market-go/domain"
+	"github.com/adenapila/market-go/logger"
 	"github.com/adenapila/market-go/service"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
@@ -14,48 +14,51 @@ import (
 )
 
 func sanityCheck() {
+	envProps := []string{
+		"SERVER_ADDRESS",
+		"SERVER_PORT",
+		"DB_USER",
+		"DB_PASSWD",
+		"DB_ADDR",
+		"DB_PORT",
+		"DB_NAME",
+	}
+	for _, k := range envProps {
+		if os.Getenv(k) == "" {
+			logger.Error(fmt.Sprintf("Environment variable %s not defined. Terminating application...", k))
+		}
+	}
 }
-
-type Database struct {
-	host, user, password, dbname string
-	port                         int
-	db                           *sql.DB
-}
-
 func Start() {
-
 	sanityCheck()
-
 	router := mux.NewRouter()
-
 	//wiring
+	//ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
 	dbClient := getDbClient()
-	//ch := CustomerHandlers{service: service.NewCustomerService(domain.NewCustomerRepositoryStub())}
 	customerRepositoryDb := domain.NewCustomerRepositoryDb(dbClient)
+	//accountRepositoryDb := domain.NewAccountRepositoryDb(dbClient)
 	accountRepositoryDb := domain.NewAccountRepositoryDb(dbClient)
 	ch := CustomerHandlers{service.NewCustomerService(customerRepositoryDb)}
 	ah := AccountHandler{service.NewAccountService(accountRepositoryDb)}
 
-	//define routes - matcher
+	// define routes
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}/account", ah.NewAccount).Methods(http.MethodPost)
 
-	//starting server
+	// starting server
 	address := os.Getenv("SERVER_ADDRESS")
 	port := os.Getenv("SERVER_PORT")
-	//log.Fatal(http.ListenAndServe("localhost:8888", router))
+	logger.Info(fmt.Sprintf("Starting server on %s:%s ...", address, port))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
 }
-
 func getDbClient() *sqlx.DB {
 	dbUser := os.Getenv("DB_USER")
 	dbPasswd := os.Getenv("DB_PASSWD")
 	dbAddr := os.Getenv("DB_ADDR")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
-
-	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPasswd, dbPort, dbName, dbAddr)
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPasswd, dbAddr, dbPort, dbName)
 	client, err := sqlx.Open("mysql", dataSource)
 	if err != nil {
 		panic(err)
